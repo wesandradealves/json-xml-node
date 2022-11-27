f63d97a82ee1b977863c5c6b3e593ce8d0028eca
Wesley Alves <wesley.alves@actdigital.com>
2022-11-27 02:24:39 -0300

    .

--- index.js
+++ index.js
@@ -14,47 +14,48 @@ var server = http.createServer(function(request, response){    
     response.setHeader("X-Download-Options", "noopen");
 
     if (request.url == "/") {
-        let body = '';
-        let file = '';
+        // let body = '';
+        // let file = '';
         
-        request.on('data', chunk => {
-            let json = JSON.parse(chunk);
-            const slugify = require('slugify');
+        // request.on('data', chunk => {
+        //     let json = JSON.parse(chunk);
+        //     const slugify = require('slugify');
     
-            const header = Object.entries(json.data).map((item) => {
-                return item[1][0]
-            }, []);
+        //     const header = Object.entries(json.data).map((item) => {
+        //         return item[1][0]
+        //     }, []);
 
-            const filename = slugify(json.municipio, {
-                replacement: '-',  // replace spaces with replacement character, defaults to `-`
-                remove: undefined, // remove characters that match regex, defaults to `undefined`
-                lower: true,      // convert to lower case, defaults to `false`
-                strict: true,     // strip special characters except replacement, defaults to `false`
-                trim: true         // trim leading and trailing replacement chars, defaults to `true`
-            });
+        //     const filename = slugify(json.municipio, {
+        //         replacement: '-',  // replace spaces with replacement character, defaults to `-`
+        //         remove: undefined, // remove characters that match regex, defaults to `undefined`
+        //         lower: true,      // convert to lower case, defaults to `false`
+        //         strict: true,     // strip special characters except replacement, defaults to `false`
+        //         trim: true         // trim leading and trailing replacement chars, defaults to `true`
+        //     });
 
-            const info = json.data.map(row => {
-                return row[1]
-            });    
+        //     const info = json.data.map(row => {
+        //         return row[1]
+        //     });
             
-            const workBook = XLSX.utils.book_new();
-            const workSheetData = [
-                header,
-                [...info]
-            ];
+        //     const workBook = XLSX.utils.book_new();
+        //     const workSheetData = [
+        //         header,
+        //         [...info]
+        //     ];
 
-            const workSheet = XLSX.utils.aoa_to_sheet(workSheetData);
-            XLSX.utils.book_append_sheet(workBook, workSheet, filename);
-            // XLSX.writeFile(workBook, path.resolve(`./${filename}.xlsx`));
+        //     const workSheet = XLSX.utils.aoa_to_sheet(workSheetData);
+        //     XLSX.utils.book_append_sheet(workBook, workSheet, filename);
+        //     // XLSX.writeFile(workBook, path.resolve(`./${filename}.xlsx`));
             
-            file = XLSX.write(workBook, { type: 'base64' })
-            // body += chunk.toString();    
-            return true;            
-        });
+        //     file = XLSX.write(workBook, { type: 'base64' })
+        //     // body += chunk.toString();
+        //     return true;
+        // });
 
-        request.on('end', () => {
-            response.end(JSON.stringify({ file: file }));
-        });     
+        // request.on('end', () => {
+        //     response.end(JSON.stringify({ file: file }));
+        // });     
+        response.end('It works!');
     }  
 }); 
 
